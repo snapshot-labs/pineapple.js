c3928d5c162b53240b1357b398ec923bb72c14e2
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-09 00:43:36 +0900

    fix: return error response from pineapple

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,29 @@
-import fetch from 'cross-fetch';
+import fetch from "cross-fetch";
 
-const PINEAPPLE_URL = 'https://pineapple.fyi';
+const PINEAPPLE_URL = "https://pineapple.fyi";
 
 export async function pin(json: any, url: string = PINEAPPLE_URL) {
   const init = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      jsonrpc: '2.0',
-      method: 'pin',
+      jsonrpc: "2.0",
+      method: "pin",
       params: json,
-      id: null
-    })
+      id: null,
+    }),
   };
   const res = await fetch(url, init);
-  return (await res.json()).result;
+  const content = await res.json();
+  return content.result || content.error;
 }
 
 export async function upload(body: any, url = `${PINEAPPLE_URL}/upload`) {
-  const init = { method: 'POST', body };
+  const init = { method: "POST", body };
   const res = await fetch(url, init);
-  return (await res.json()).result;
+  const content = await res.json();
+  return content.result || content.error;
 }
---
+++ test/pin.test.ts
@@ -0,0 +1,12 @@
+import { pin } from "../src";
+
+describe("pin()", () => {
+  it("pins and return the provider and cid", async () => {
+    const json = { name: "Vitalik" };
+    const receipt = await pin(json);
+    expect(receipt.provider).toBe("4everland");
+    expect(receipt.cid).toBe(
+      "bafkreibatgmdqdxsair3j52zfhtntegshtypq2qbex3fgtorwx34kzippe",
+    );
+  }, 30e3);
+});
