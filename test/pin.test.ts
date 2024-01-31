f911598f3555716c440de20c1544fe4b54cf7c87
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-01-31 13:00:32 +0700

    chore(deps): dependencies update

--- test/pin.test.ts
+++ test/pin.test.ts
@@ -5,6 +5,8 @@ describe('pin()', () => {
     const json = { name: 'Vitalik' };
     const receipt = await pin(json);
     expect(receipt.provider).toBe('4everland');
-    expect(receipt.cid).toBe('bafkreibatgmdqdxsair3j52zfhtntegshtypq2qbex3fgtorwx34kzippe');
+    expect(receipt.cid).toBe(
+      'bafkreibatgmdqdxsair3j52zfhtntegshtypq2qbex3fgtorwx34kzippe'
+    );
   });
 });
