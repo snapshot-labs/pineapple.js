da40e2ca9f4ac3f997c8eaffa5774a83621876ed
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-07-24 19:09:17 +0400

    test: upload test for new expected image dimensions

--- test/upload.test.ts
+++ test/upload.test.ts
@@ -10,7 +10,7 @@ describe('upload()', () => {
       formData.append('file', createReadStream(path.join(__dirname, './fixtures/valid.png')));
       const receipt = await upload(formData);
       expect(receipt.provider).toBe('4everland');
-      expect(receipt.cid).toBe('bafkreidxvfyqu6l3tb3y5gi2nq5zqyincpev2rangnv7nmaocrk7q3o2fi');
+      expect(receipt.cid).toBe('bafkreif6ai5u636rsxcyk45bqmmt263h35hpqakxays4pdzkfdbsdw7kva');
     });
   });
 
